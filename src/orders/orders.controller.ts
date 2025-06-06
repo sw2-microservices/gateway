cfd759fa3a837aaf67d16721ccb47f336ebdbbb3
jherson <rodriguezpaul98@gmail.com>
2025-06-06 18:23:31 -0400

    findOne implemented

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
 import { CreateOrderDto } from './dto';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
@@ -20,8 +21,18 @@ export class OrdersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersClient.send('findOneOrder', { id });
+  async findOne(@Param('id') id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id })
+      );
+
+      return order;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+
   }
 
 }
