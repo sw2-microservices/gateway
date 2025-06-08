7aa8cf8056965104d55ba7131d8ebff15063bfd8
jherson <rodriguezpaul98@gmail.com>
2025-06-07 21:51:03 -0400

    add auth connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AircraftModule } from './aircraft/aircraft.module';
 import { OrdersModule } from './orders/orders.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { NatsModule } from './transports/nats.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AircraftModule, OrdersModule, ReservationModule, NatsModule],
+  imports: [AircraftModule, OrdersModule, ReservationModule, NatsModule, AuthModule],
 })
 export class AppModule {}
